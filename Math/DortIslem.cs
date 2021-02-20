2d99cb01f05a6ead9589559bd6fba5e2863389ec
EgeKaratas <79366986+EgeKaratas@users.noreply.github.com>
2021-02-20 17:06:49 +0300

    Add files via upload

---
+++ Math/DortIslem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Math
+{
+    class DortIslem
+    {
+       public void Topla(int x,int y)
+        {
+            int toplam = x + y;
+
+            Console.WriteLine("Sonuc :" +toplam);
+
+        }
+
+    }
+}
