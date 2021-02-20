3aedd9d12c16516d6706cbf81f1c38c071245596
EgeKaratas <79366986+EgeKaratas@users.noreply.github.com>
2021-02-20 17:07:50 +0300

    Add files via upload

---
+++ methods/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "elma";
+            double fiyat = 15;
+            string aciklama = "Amasya Elması";
+
+            Product product1 = new Product();
+            product1.Name = "Elma";
+            product1.Price = 15;
+            product1.Explanation = "Amasya Elması";
+
+            Product product2 = new Product();
+            product2.Name = "Portakal";
+            product2.Price = 7;
+            product2.Explanation = "Washington Portakalı";
+
+
+
+            Product[] products = new Product[] { product1, product2 };
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.Price);
+                Console.WriteLine(product.Explanation);
+                Console.WriteLine("--------------------");
+
+            }
+
+            Console.WriteLine("----------Metotlar---------");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(product1);
+            sepetManager.Ekle(product2);
+           
+        }
+    }
+}
---
+++ methods/SepetManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace methods
+{
+    class SepetManager
+    {
+        public void Ekle(Product product)
+        {
+            Console.WriteLine("Sepete eklendi :" +product.Name);
+
+        }
+
+        public void Ekle2(string)
+        {
+
+        }
+    }
+}
